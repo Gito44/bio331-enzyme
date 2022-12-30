d8dc63e075bfe1910105e6431e7d1c7a0c7ff12f
Zühtü Anıl Tutar <121525523+Gito44@users.noreply.github.com>
2022-12-30 16:50:22 +0300

    Add files via upload

--- enzyme activity and graphs.py
+++ enzyme activity and graphs.py
@@ -7,16 +7,18 @@ tube1= [0.176, 0.217, 0.242, 0.545, 0.971, 1.430, 2.283]
 tube2= [0.160, 0.205, 0.246, 0.537, 0.955, 1.385, 2.201]
 tube3= [0.164, 0.221, 0.250, 0.565, 0.959, 1.369, 2.119]
 tube4= [0.155, 0.205, 0.250, 0.516, 0.951, 1.363, 2.164]
-enzyme_concs=[0.002,0.004,0.006,0.008]
+enzyme_concs=[0.67,1.33,2.00,2.67]
 
 def enzyme_activity(data):
     enzyme_a=0
     count = 0
+
     for i in range(len(data)):
         activity=data[i]/times[i]
         enzyme_a+=activity
         count+=1
-    return enzyme_a/count
+
+    return enzyme_a/count*10000
 
 V1= enzyme_activity(tube1)
 V2= enzyme_activity(tube2)
@@ -34,8 +36,8 @@ trendx= np.linspace(0,df["concs"].max(),100)
 trendy= np.polyval(trend,trendx)
 plt.plot( df["concs"],df["activities"], "bo", label="Data")
 plt.plot(trendx,trendy, "r--", label="Trendline")
-plt.xlabel("Enzyme concentration (enzyme volume/total volume)")
-plt.ylabel("Enzyme activity")
+plt.xlabel("Enzyme concentration (uL/mL)")
+plt.ylabel("Enzyme activity (10e-4) (uL/second)")
 plt.legend(loc="best")
 plt.title("Enzyme Activity of Different Enzyme Concentrations")
 plt.show()
